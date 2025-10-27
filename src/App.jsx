af42df1233b40edf3c9bf249433a241c45720be4
shivani <jaiswalShivani432@gmail.com>
2025-10-27 19:38:20 +0530

    created leftsidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import LeftSidebar from './coponenets/leftSidebar'
 function App() {
-  
+  return(
+    <>
+     <LeftSidebar/>
+    </>
+  )
+
 }
 
 export default App
